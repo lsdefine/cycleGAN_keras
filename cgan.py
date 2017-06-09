0af15564669f5e230a59188251f4bb64da321c55
LJQ <l.j.q.light@gmail.com>
2017-06-09 17:24:26 +0800

    Add files via upload
    create

---
+++ cgan.py
@@ -0,0 +1,152 @@
+﻿import ljqpy, os, sys, time, random
+import numpy as np
+import keras.backend as K
+from collections import defaultdict
+from keras.datasets import mnist
+from keras.utils.generic_utils import Progbar
+from keras.optimizers import Adam
+from keras.models import *
+from keras.layers import *
+from keras.preprocessing import image
+from PIL import Image
+time.clock()
+
+np.random.seed(1333)
+K.set_image_dim_ordering('tf')
+
+from model_256 import BuildGenerator, BuildDiscriminator
+
+# params
+nb_epochs = 200
+batch_size = 1
+p_lambda = 20
+
+channels = 3
+imgsize = 256
+
+adam_lr = 0.00001
+adam_beta_1 = 0.5
+
+imgdirA = '/mnt/smb25/ImageNet/horse2zebra/trainA'
+imgdirB = '/mnt/smb25/ImageNet/horse2zebra/trainB'
+testimgdirA = '/mnt/smb25/ImageNet/horse2zebra/testA'
+testimgdirB = '/mnt/smb25/ImageNet/horse2zebra/testB'
+
+modeldir = 'data/'
+testimgdir = 'images/'
+
+
+modelG = BuildGenerator()
+modelF = BuildGenerator()
+modelDG = BuildDiscriminator()
+modelDF = BuildDiscriminator()
+
+modelG.summary()
+modelDG.summary()
+
+try:
+	modelG.load_weights( os.path.join(modeldir, 'modelG.h5' ))
+	modelF.load_weights( os.path.join(modeldir, 'modelF.h5' ))
+	modelDG.load_weights(os.path.join(modeldir, 'modelDG.h5'))
+	modelDF.load_weights(os.path.join(modeldir, 'modelDF.h5'))	
+except Exception as e: 
+	print(e)
+
+modelDG.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
+modelDF.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
+modelG.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
+modelF.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
+
+imageA = Input(shape=(imgsize,imgsize,channels))
+imageB = Input(shape=(imgsize,imgsize,channels))
+modelDG.trainable = False
+modelDF.trainable = False
+fakeB = modelG(imageA)
+fakeA = modelF(imageB)
+disG = modelDG(fakeB)
+disF = modelDF(fakeA)
+cycGF = modelF(fakeB)
+cycFG = modelG(fakeA)
+combM = Model(inputs=[imageA, imageB], outputs=[disG, disF, cycGF, cycFG])
+combM.compile(optimizer=Adam(adam_lr, adam_beta_1), loss=['mse','mse','mae','mae'], loss_weights=[1, 1, p_lambda, p_lambda])
+
+def ImgGenerator(imgdir):
+	imglst = [os.path.join(imgdir, x) for x in os.listdir(imgdir)]
+	while True:
+		random.shuffle(imglst)
+		for fn in imglst:
+			img = image.load_img(fn, target_size=(imgsize, imgsize))
+			img = (image.img_to_array(img) - 127.5) / 127.5
+			yield np.expand_dims(img, axis=0)
+
+# if the memory is enough for all imgs ...
+def ImgGeneratorS(imgdir):
+	imglst = [os.path.join(imgdir, x) for x in os.listdir(imgdir)]
+	X = np.zeros((len(imglst), imgsize, imgsize, channels))
+	for i, fn in enumerate(imglst):
+		if i % 20 == 0: print('%d/%d' % (i, len(imglst)))
+		img = image.load_img(fn, target_size=(imgsize, imgsize))
+		X[i] = image.img_to_array(img)
+	X = (X - 127.5) / 127.5
+	ids = list(range(len(imglst)))
+	while True:
+		random.shuffle(ids)
+		for ii in ids: yield X[ii:ii+1]
+
+genA = ImgGeneratorS(imgdirA)
+genB = ImgGeneratorS(imgdirB)
+testA = ImgGenerator(testimgdirA)
+testB = ImgGenerator(testimgdirB)
+
+nb_batches = len(os.listdir(imgdirA)) // batch_size
+ones = np.ones(   (batch_size, imgsize//8, imgsize//8, 1) )
+zeros = np.zeros( (batch_size, imgsize//8, imgsize//8, 1) )
+
+
+recordG, recordF = [], []
+for epoch in range(nb_epochs):
+	print('Epoch %d of %d' % (epoch+1, nb_epochs))
+	progress_bar = Progbar(target=nb_batches)
+	
+	lossDG, lossDF = 0.5, 0.5
+	for index in range(nb_batches):
+		A_image_batch, B_image_batch = next(genA), next(genB)
+
+		generateG = modelG.predict_on_batch(A_image_batch)
+		recordG.append(generateG)
+		if len(recordG) > 100: recordG = recordG[-50:]
+		lossDG = modelDG.train_on_batch(B_image_batch, ones)
+		lossDG += modelDG.train_on_batch(random.choice(recordG), zeros)
+		lossDG *= 0.5
+
+		generateF = modelF.predict_on_batch(B_image_batch)
+		recordF.append(generateF)
+		if len(recordF) > 100: recordF = recordF[-50:]
+		lossDF = modelDF.train_on_batch(A_image_batch, ones)
+		lossDF += modelDF.train_on_batch(random.choice(recordF), zeros)
+		lossDF *= 0.5
+
+		for _ in range(1):
+			A_image_batch, B_image_batch = next(genA), next(genB)
+			_, lossG, lossF, losscycGF, losscycFG = combM.train_on_batch([A_image_batch, B_image_batch], [ones, ones, A_image_batch, B_image_batch])
+
+		progress_bar.update(index+1, values=[('DG',lossDG),('G',lossG),('DF',lossDF),('F',lossF),('cycGF',losscycGF),('cycFG',losscycFG)])
+
+	print('Testing for epoch {}:'.format(epoch + 1))
+
+	modelG.save_weights( os.path.join(modeldir, 'modelG.h5' ),  True)
+	modelF.save_weights( os.path.join(modeldir, 'modelF.h5' ),  True)
+	modelDG.save_weights(os.path.join(modeldir, 'modelDG.h5'), True)
+	modelDF.save_weights(os.path.join(modeldir, 'modelDF.h5'), True)
+	
+	tA = np.concatenate( [next(testA) for x in range(4)], axis=0 ) 
+	tB = np.concatenate( [next(testB) for x in range(4)], axis=0 ) 
+	gG = modelG.predict(tA, batch_size=1)
+	gF = modelF.predict(tB, batch_size=1)
+	tA = tA.reshape(-1,imgsize,channels)
+	tB = tB.reshape(-1,imgsize,channels)
+	gG = gG.reshape(-1,imgsize,channels)
+	gF = gF.reshape(-1,imgsize,channels)
+	img = np.concatenate([tA, gG, tB, gF], axis=1)
+	img = (img * 127.5 + 127.5).astype(np.uint8)  
+	Image.fromarray(img).save(os.path.join(testimgdir, 'plot_epoch_{0:03d}_generated.png'.format(epoch)))
---
+++ model_256.py
@@ -0,0 +1,73 @@
+﻿from keras.layers import *
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import *
+from keras.models import *
+from keras.initializers import RandomNormal
+import keras.backend as K
+import tensorflow as tf
+
+
+def ResBlock(x, dim, kernelsize=3):
+	y = Conv2D(dim, kernelsize, padding='same')(x)
+	y = LeakyReLU()(y)
+	y = Conv2D(dim, kernelsize, padding='same')(y)
+	y = LeakyReLU()(y)
+	return add([x,y])
+
+# batch_size=1, InstNorm = BatchNorm?
+def InstanceNorm(x):
+	mean, var = tf.nn.moments(x, axes=[1,2])
+	epsilon = 1e-5
+	return (x - mean) * tf.rsqrt(var + epsilon)
+
+def ResizeConv(x, cdim):
+	def Resize2x(x):
+		shape = x.get_shape()
+		new_shape = tf.shape(x)[1:3] * tf.constant(np.array([2, 2], dtype='int32'))
+		x = tf.image.resize_bilinear(x, new_shape)
+		x.set_shape( (None, shape[1]*2, shape[2]*2, None) )
+		return x
+	x = Lambda(Resize2x)(x)
+	x = Conv2D(cdim, 3, padding='same')(x)
+	return x
+
+def BuildGenerator():
+	gdim = 32
+	image = Input(shape=(256,256,3))
+	x = Conv2D(gdim,   7, strides=1, padding='same')(image)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	x = Conv2D(gdim*2, 3, strides=2, padding='same')(x)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	x = Conv2D(gdim*4, 3, strides=2, padding='same')(x)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	for ii in range(9): x = ResBlock(x, gdim*4)
+	x = ResizeConv(x, gdim*2)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	x = ResizeConv(x, gdim)
+	x = BatchNormalization()(x)
+	x = LeakyReLU()(x)
+	x = Conv2D(3, 3, strides=1, padding='same', activation='tanh')(x)
+	return Model(inputs=image, outputs=x)
+
+def BuildDiscriminator():
+	cnn = Sequential()
+	ddim = 64
+	cnn.add(Conv2D(ddim, 7, padding='same', strides=2, input_shape=(256,256,3)))
+	cnn.add(BatchNormalization())
+	cnn.add(LeakyReLU())
+	cnn.add(Conv2D(ddim*2, 3, padding='same', strides=2))
+	cnn.add(BatchNormalization())
+	cnn.add(LeakyReLU())
+	cnn.add(Conv2D(ddim*4, 3, padding='same', strides=2))
+	cnn.add(BatchNormalization())
+	cnn.add(LeakyReLU())
+	cnn.add(Conv2D(ddim*8, 3, padding='same', strides=1))
+	cnn.add(BatchNormalization())
+	cnn.add(LeakyReLU()) 
+	cnn.add(Conv2D(1, 3, padding='same', strides=1))
+	image = Input(shape=(256,256,3))
+	return Model(inputs=image, outputs=cnn(image))
